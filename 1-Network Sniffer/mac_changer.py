05efa12f9e407d800fca4f2df72e4e39693c8823
root <root@localhost.localdomain>
2019-08-15 02:23:25 +0530

    Mac manual setup1

--- 1-Network Sniffer/mac_changer.py
+++ 1-Network Sniffer/mac_changer.py
@@ -3,3 +3,7 @@
 import subprocess
 
 subprocess.call("ifconfig", shell=True)
+subprocess.call("ifconfig eth0 down ", shell=True)
+subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:55:66 ", shell=True)
+subprocess.call("ifconfig eth0 up ", shell=True)
+
